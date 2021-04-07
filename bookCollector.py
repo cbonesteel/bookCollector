e2067e4172e32a8af6dfa7d405354276934b7402
cbonesteel <cambones12@gmail.com>
2021-04-07 14:09:28 -0400

    updated bookCollector to parse into database and ran it

--- bookCollector.py
+++ bookCollector.py
@@ -1,4 +1,5 @@
 import requests
+import mysql.connector
 from random import randrange
 import databaseConnector as db
 import pandas
@@ -18,77 +19,78 @@ for k in isbns:
 
     dictionary = response.json()
 
-    bookID = 17
+    try:
+        for i in dictionary.get("items"):
+            try:
+                # Gets standard book information (i.e. title, authors, etc.)
+                if isinstance(i.get("volumeInfo").get("categories"), list):
+                    cat = ""
+                    for j in i.get("volumeInfo").get("categories"):
+                        cat += j + " "
+                else:
+                    cat = "None"
 
-    # books = open('books.txt', 'w')
-    # inventory = open('inventory.txt', 'w')
+                authors = ""
+                for j in i.get("volumeInfo").get("authors"):
+                    authors += j + " "
 
-    for i in dictionary.get("items"):
-        # Gets standard book information (i.e. title, authors, etc.)
-        if isinstance(i.get("volumeInfo").get("categories"), list):
-            cat = ""
-            for j in i.get("volumeInfo").get("categories"):
-                cat += j + " "
-            else:
-                cat = "None"
-            
-        authors = ""
-        for j in i.get("volumeInfo").get("authors"):
-            authors += j + " "
+                if i.get("volumeInfo").get("edition") == "null":
+                    edition = 1
+                else:
+                    edition = i.get("volumeInfo").get("edition")
 
-            if i.get("volumeInfo").get("edition") == "null":
-                edition = 1
-            else:
-                edition = i.get("volumeInfo").get("edition")
+                ISBN = i.get("volumeInfo").get("industryIdentifiers")[0].get("identifier")
+                title = i.get("volumeInfo").get("title")
+                publisher = i.get("volumeInfo").get("publisher")
+                date = i.get("volumeInfo").get("publishedDate")
+                rating = i.get("volumeInfo").get("averageRating")
+                cover = i.get("volumeInfo").get("imageLinks").get("thumbnail")
+                description = i.get("volumeInfo").get("description")
 
-        ISBN = i.get("volumeInfo").get("industryIdentifiers")[0].get("identifier")
-        title = i.get("volumeInfo").get("title")
-        publisher = i.get("volumeInfo").get("publisher")
-        date = i.get("volumeInfo").get("publishedDate")
-        rating = i.get("volumeInfo").get("averageRating")
-        cover = i.get("volumeInfo").get("imageLinks").get("thumbnail")
-        description = i.get("volumeInfo").get("description")
+                if len(ISBN) > 13:
+                    raise TypeError
 
-        print(ISBN)
-        
-        db.query(
-            "INSERT INTO Books (ISBN, Category, Author, Title, Edition, Publisher, PublicationYear, Rating, CoverPicture, Description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
-            (ISBN, cat, authors, title, edition, publisher, date[0:4], rating, cover, description)
-        )
+                quantity = randrange(50)
 
-        db.commit()
-    
-        # Prints standard book information to books text file
-        # print('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %
-        #       (ISBN, cat, authors, title, edition, publisher, date[0:4], rating, cover, description), file=books)
+                if quantity == 0:
+                    available = "1"
+                else:
+                    available = "0"
 
-        # Gets inventory information for a book (i.e. prices, quantity in stock, etc.)
-        quantity = randrange(50)
-        
-        if quantity == 0:
-            available = "1"
-        else:
-            available = "0"
-            
-        if type(i.get("saleInfo").get("retailPrice")) == dict:
-            buyPrice = i.get("saleInfo").get("retailPrice").get("amount")
-        else:
-            buyPrice = 9.99
-            
-        if type(i.get("saleInfo").get("listPrice")) == dict:
-            sellPrice = i.get("saleInfo").get("listPrice").get("amount")
-        else:
-            sellPrice = 19.99
+                if type(i.get("saleInfo").get("retailPrice")) == dict:
+                    buyPrice = i.get("saleInfo").get("retailPrice").get("amount")
+                else:
+                    buyPrice = 9.99
 
-        db.query(
-            "INSERT INTO Inventory (BookID, Quantity, MinThreshold, BuyPrice, SellPrice, BookStatus) VALUES (%s, %s, %s, %s, %s, %s)",
-            (bookID, quantity, "2", buyPrice, sellPrice, available)
-        )
+                if type(i.get("saleInfo").get("listPrice")) == dict:
+                    sellPrice = i.get("saleInfo").get("listPrice").get("amount")
+                else:
+                    sellPrice = 19.99
 
-        db.commit()
-    
-        # Prints inventory information to inventory text file
-        # print('%s\t%s\t%s\t%s\t%s\t%s' % (bookID, quantity, "2", buyPrice, sellPrice, available), file=inventory)
+                db.query(
+                    "INSERT INTO Books (ISBN, Category, Author, Title, Edition, Publisher, PublicationYear, Rating, CoverPicture, Description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                    (ISBN, cat, authors, title, edition, publisher, date[0:4], rating, cover, description)
+                )
 
-        # increments bookID for inventory information foreign key
-        bookID = bookID + 1
+                selectID = "SELECT BookID FROM Books WHERE ISBN={isbn}".format(isbn=ISBN)
+
+                bookID = db.query(selectID)
+
+                db.query(
+                    "INSERT INTO Inventory (BookID, Quantity, MinThreshold, BuyPrice, SellPrice, BookStatus) VALUES (%s, %s, %s, %s, %s, %s)",
+                    (bookID[0][0], quantity, "2", buyPrice, sellPrice, available)
+                )
+
+                db.commit()
+
+                # increments bookID for inventory information foreign key
+            except KeyError:
+                print("Key Error, moving to next book")
+            except AttributeError:
+                print("Attribute Error, moving to next book")
+            except TypeError:
+                print("Type Error, moving to next book")
+            except mysql.connector.Error as err:
+                print("Something went wrong: {}".format(err))
+    except TypeError:
+        print("Type Error, moving to next book")
